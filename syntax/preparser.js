098eab49d1b7d2c5e034bba432a544140dfac07b
Ray Toal <rtoal@lmu.edu>
2017-03-26 10:34:13 -0700

    Preparser now throws errors not strings

--- syntax/preparser.js
+++ syntax/preparser.js
@@ -4,9 +4,10 @@
  *   const withIndentsAndDedents = require('./preparser');
  *
  *   withIndentsAndDedents(text)
- *       returns text with ⇨ characters replacing indent regions and ⇦
- *       replacing dedents. If the program text is misindented, the
- *       function will throw an error message.
+ *       Returns text with ⇨ characters replacing indent regions and ⇦
+ *       replacing dedents. If the program text is misindented, or has
+ *       any whitespace other than a space character in the indentation
+ *       region, the function will throw an error.
  */
 
 module.exports = (text) => {
@@ -18,7 +19,7 @@ module.exports = (text) => {
     if (content === '') {
       result.push('');
     } else if (/\s/.test(content[0])) {
-      throw 'Illegal whitespace character';
+      throw new Error('Illegal whitespace character');
     } else if (indent === stack[stack.length - 1]) {
       result.push(content);
     } else if (indent > stack[stack.length - 1]) {
@@ -28,7 +29,7 @@ module.exports = (text) => {
       for (let dedents = 1; true; dedents += 1) {
         const next = (stack.pop(), stack[stack.length - 1]);
         if (indent > next) {
-          throw 'Indent Error';
+          throw new Error('Indent Error');
         } else if (indent === next) {
           result.push(`${'⇦'.repeat(dedents)}${content}`);
           break;
