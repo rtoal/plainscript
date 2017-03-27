cc0142b032dc302064a9987180e3251b2f613160
Ray Toal <rtoal@lmu.edu>
2017-03-26 21:39:07 -0700

    Size check for assignment statements

--- ast/assignment-statement.js
+++ ast/assignment-statement.js
@@ -5,6 +5,9 @@ module.exports = class AssignmentStatement {
   }
 
   analyze(context) {
+    if (this.targets.length !== this.sources.length) {
+      throw new Error('Number of variables does not equal number of expressions');
+    }
     this.targets.forEach(v => v.analyze(context));
     this.sources.forEach(e => e.analyze(context));
   }
