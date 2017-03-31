ad79e34945df18923135692da8263fbc94db6c89
Ray Toal <rtoal@lmu.edu>
2017-03-30 22:03:31 -0700

    Cleaner analyzer test

--- test/analyze-test.js
+++ test/analyze-test.js
@@ -7,10 +7,10 @@ const TEST_DIR = './test/analyzer-checks';
 describe('The semantic analyzer', () => {
   fs.readdirSync(TEST_DIR).forEach((name) => {
     if (name.endsWith('.error')) {
-      it(`detects an error in ${name}`, (done) => {
+      it(`detects a ${name.replace(/[^a-z]/g, ' ')}`, (done) => {
         const program = parse(fs.readFileSync(`${TEST_DIR}/${name}`, 'utf-8'));
-        assert.throws(() => program.analyze(),
-          new RegExp(name.replace('.error', '').replace(/-/g, ' ')));
+        const errorPattern = RegExp(name.replace('.error', '').replace(/-/g, ' '), 'i');
+        assert.throws(() => program.analyze(), errorPattern);
         done();
       });
     }
