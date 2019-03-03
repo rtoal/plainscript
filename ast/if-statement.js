7ee81bd632ce6184d9e77ff2a8f64bdde995f57d
Ray Toal <rtoal@lmu.edu>
2019-03-02 17:54:37 -0800

    Refactor IfStmts so special Case is not needed

--- ast/if-statement.js
+++ ast/if-statement.js
@@ -1,17 +1,26 @@
 module.exports = class IfStatement {
-  constructor(cases, alternate) {
-    Object.assign(this, { cases, alternate });
+  constructor(tests, consequents, alternate) {
+    Object.assign(this, { tests, consequents, alternate });
   }
 
   analyze(context) {
-    this.cases.forEach(c => c.analyze(context.createChildContextForBlock()));
+    this.tests.forEach(test => test.analyze(context));
+    this.consequents.forEach((block) => {
+      const blockContext = context.createChildContextForBlock();
+      block.forEach(statement => statement.analyze(blockContext));
+    });
     if (this.alternate) {
       this.alternate.forEach(s => s.analyze(context.createChildContextForBlock()));
     }
   }
 
   optimize() {
-    this.cases.map(s => s.optimize()).filter(s => s !== null);
+    this.tests.map(test => test.optimize());
+    // Suggested: for a false test, remove the corresponding consequent
+    this.consequents.forEach((block) => {
+      block.map(statement => statement.optimize().filter(s => s != null));
+      // Suggested: Look for breaks/returns in the middle of the body
+    });
     this.alternate = this.alternate ? this.alternate.optimize() : null;
     return this;
   }
--- syntax/parser.js
+++ syntax/parser.js
@@ -20,7 +20,6 @@ const AssignmentStatement = require('../ast/assignment-statement');
 const BreakStatement = require('../ast/break-statement');
 const ReturnStatement = require('../ast/return-statement');
 const IfStatement = require('../ast/if-statement');
-const Case = require('../ast/case');
 const WhileStatement = require('../ast/while-statement');
 const CallStatement = require('../ast/call-statement');
 const FunctionDeclaration = require('../ast/function-declaration');
@@ -39,51 +38,111 @@ const StringLiteral = require('../ast/string-literal');
 const grammar = ohm.grammar(fs.readFileSync('./syntax/plainscript.ohm'));
 
 // Ohm turns `x?` into either [x] or [], which we should clean up for our AST.
-function unpack(a) {
+function arrayToNullable(a) {
   return a.length === 0 ? null : a[0];
 }
 
 /* eslint-disable no-unused-vars */
 const astGenerator = grammar.createSemantics().addOperation('ast', {
-  Program(_1, body, _2) { return new Program(body.ast()); },
-  Stmt_simple(statement, _) { return statement.ast(); },
-  Stmt_while(_, test, suite) { return new WhileStatement(test.ast(), suite.ast()); },
+  Program(_1, body, _2) {
+    return new Program(body.ast());
+  },
+  Stmt_simple(statement, _) {
+    return statement.ast();
+  },
+  Stmt_while(_, test, suite) {
+    return new WhileStatement(test.ast(), suite.ast());
+  },
   Stmt_if(_1, firstTest, firstSuite, _2, moreTests, moreSuites, _3, lastSuite) {
     const tests = [firstTest.ast(), ...moreTests.ast()];
-    const bodies = [firstSuite.ast(), ...moreSuites.ast()];
-    const cases = tests.map((test, index) => new Case(test, bodies[index]));
-    return new IfStatement(cases, unpack(lastSuite.ast()));
+    const consequents = [firstSuite.ast(), ...moreSuites.ast()];
+    const alternate = arrayToNullable(lastSuite.ast());
+    return new IfStatement(tests, consequents, alternate);
   },
   Stmt_def(_1, id, _2, params, _3, suite) {
     return new FunctionDeclaration(id.ast(), params.ast(), suite.ast());
   },
-  SimpleStmt_vardecl(_1, v, _2, e) { return new VariableDeclaration(v.ast(), e.ast()); },
-  SimpleStmt_assign(v, _, e) { return new AssignmentStatement(v.ast(), e.ast()); },
-  SimpleStmt_break(_) { return new BreakStatement(); },
-  SimpleStmt_return(_, e) { return new ReturnStatement(unpack(e.ast())); },
-  SimpleStmt_call(c) { return new CallStatement(c.ast()); },
-  Suite_small(_1, statement, _2) { return [statement.ast()]; },
-  Suite_large(_1, _2, _3, statements, _4) { return statements.ast(); },
-  Exp_or(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp_and(left, op, right) { return new BinaryExpression(op.ast(), right.ast()); },
-  Exp1_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp2_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp3_binary(left, op, right) { return new BinaryExpression(op.ast(), left.ast(), right.ast()); },
-  Exp4_unary(op, operand) { return new UnaryExpression(op.ast(), operand.ast()); },
-  Exp5_list(_1, expressions, _2) { return new ListExpression(expressions.ast()); },
-  Exp5_parens(_1, expression, _2) { return expression.ast(); },
-  Call(callee, _1, args, _2) { return new Call(callee.ast(), args.ast()); },
-  VarExp_subscripted(v, _1, e, _2) { return new SubscriptedExpression(v.ast(), e.ast()); },
-  VarExp_simple(id) { return new IdentifierExpression(id.ast()); },
-  Param(id, _, exp) { return new Parameter(id.ast(), unpack(exp.ast())); },
-  Arg(id, _, exp) { return new Argument(unpack(id.ast()), exp.ast()); },
-  NonemptyListOf(first, _, rest) { return [first.ast(), ...rest.ast()]; },
-  EmptyListOf() { return []; },
-  boollit(_) { return new BooleanLiteral(!!this.sourceString); },
-  numlit(_1, _2, _3, _4, _5, _6) { return new NumericLiteral(+this.sourceString); },
-  strlit(_1, chars, _6) { return new StringLiteral(this.sourceString); },
-  id(_1, _2) { return this.sourceString; },
-  _terminal() { return this.sourceString; },
+  SimpleStmt_vardecl(_1, v, _2, e) {
+    return new VariableDeclaration(v.ast(), e.ast());
+  },
+  SimpleStmt_assign(v, _, e) {
+    return new AssignmentStatement(v.ast(), e.ast());
+  },
+  SimpleStmt_break(_) {
+    return new BreakStatement();
+  },
+  SimpleStmt_return(_, e) {
+    return new ReturnStatement(arrayToNullable(e.ast()));
+  },
+  SimpleStmt_call(c) {
+    return new CallStatement(c.ast());
+  },
+  Suite_small(_1, statement, _2) {
+    return [statement.ast()];
+  },
+  Suite_large(_1, _2, _3, statements, _4) {
+    return statements.ast();
+  },
+  Exp_or(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp_and(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp1_binary(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp2_binary(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp3_binary(left, op, right) {
+    return new BinaryExpression(op.ast(), left.ast(), right.ast());
+  },
+  Exp4_unary(op, operand) {
+    return new UnaryExpression(op.ast(), operand.ast());
+  },
+  Exp5_list(_1, expressions, _2) {
+    return new ListExpression(expressions.ast());
+  },
+  Exp5_parens(_1, expression, _2) {
+    return expression.ast();
+  },
+  Call(callee, _1, args, _2) {
+    return new Call(callee.ast(), args.ast());
+  },
+  VarExp_subscripted(v, _1, e, _2) {
+    return new SubscriptedExpression(v.ast(), e.ast());
+  },
+  VarExp_simple(id) {
+    return new IdentifierExpression(id.ast());
+  },
+  Param(id, _, exp) {
+    return new Parameter(id.ast(), arrayToNullable(exp.ast()));
+  },
+  Arg(id, _, exp) {
+    return new Argument(arrayToNullable(id.ast()), exp.ast());
+  },
+  NonemptyListOf(first, _, rest) {
+    return [first.ast(), ...rest.ast()];
+  },
+  EmptyListOf() {
+    return [];
+  },
+  boollit(_) {
+    return new BooleanLiteral(!!this.sourceString);
+  },
+  numlit(_1, _2, _3, _4, _5, _6) {
+    return new NumericLiteral(+this.sourceString);
+  },
+  strlit(_1, chars, _6) {
+    return new StringLiteral(this.sourceString);
+  },
+  id(_1, _2) {
+    return this.sourceString;
+  },
+  _terminal() {
+    return this.sourceString;
+  },
 });
 /* eslint-enable no-unused-vars */
 
