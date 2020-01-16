27f5db4443fc04b47a24be17b5b79075faa111f9
Justin Torres <jtorre39@lion.lmu.edu>
2020-01-16 00:44:29 -0800

    removing built

--- built/type-definitions/ast.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Functions as a namespace for type aliases and interfaces
- * that are used commonly throughout the ast files.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
